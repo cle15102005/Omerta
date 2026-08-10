8c60820ad57fc97ff3e4841fab7edadab71c6c42
Le Viet Cuong <cle15102005@gmail.com>
2026-08-10 19:58:20 +0700

    chore: untrack bat/lnk files, add to gitignore (local dev scripts)

--- run-frontend.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0frontend"
-if not exist node_modules (
-    echo Installing frontend dependencies...
-    npm install
-)
-echo Starting frontend dev server...
-npm run dev
--- start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-
-echo Starting database...
-docker-compose up -d
-
-start "Backend" cmd /k "%~dp0run-backend.bat"
-start "Frontend" cmd /k "%~dp0run-frontend.bat"
